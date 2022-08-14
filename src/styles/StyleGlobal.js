b5d62be2ab2b6330c083454d188c47471e8b0b20
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-08-14 15:32:27 -0300

    Implementing css styling Global with styleComponents

---
+++ src/styles/StyleGlobal.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Sans-Serif;
+    background-color: rgb(215, 210, 210);
+  }
+`;
+
+export default Global;
