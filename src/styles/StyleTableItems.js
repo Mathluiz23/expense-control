181c7be1c171b88eb5ea648acb28a9fd87103e3a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-08-14 15:36:18 -0300

    Implementing css styling in TableItems file with styleComponents

---
+++ src/styles/StyleTableItems.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Tr = styled.tr``;
+
+export const Td = styled.td`
+  padding-top: 20px;
+  align-items: center;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  word-break: break-all;
+  padding: 5px;
+  background-color: rgb(235, 230, 230);
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+`;
